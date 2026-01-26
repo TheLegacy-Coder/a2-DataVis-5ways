eaeb58693467ae180bc6ca99165e97e4f0fa4c1a
TheLegacy-Coder <klaudiofushakako@gmail.com>
2026-01-26 18:02:54 -0500

    Created preliminary replications of the visualization in Python, Excel, and PowerBI. Also made note of some potential resources for R and D3.

--- all_visualizations/python_viz.py
+++ all_visualizations/python_viz.py
@@ -1,2 +1,19 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 
+# Referred to https://pandas.pydata.org/docs/reference/api/pandas.read_csv.html for reading the csv, since it's been some time
+rawPenglingsDf = pd.read_csv("C:/Users/klaud/WebstormProjects/klaudio-fusha-a2-datavis-5ways/a2-DataVis-5ways/penglings.csv")
+print(rawPenglingsDf)
+
+rawPenglingsDf = rawPenglingsDf.dropna()
+print(rawPenglingsDf)
+
+# Referred to https://www.w3schools.com/python/matplotlib_scatter.asp for making sure I was properly using plt.scatter
+flipperLength = rawPenglingsDf["flipper_length_mm"]
+bodyMass = rawPenglingsDf["body_mass_g"]
+plt.scatter(flipperLength, bodyMass)
+plt.xlabel("Flipper Length (mm)")
+plt.ylabel("Body Mass (g)")
+plt.show()
+
+# Note to self: Will color-code data point by species later, and will change point size based on bill length
